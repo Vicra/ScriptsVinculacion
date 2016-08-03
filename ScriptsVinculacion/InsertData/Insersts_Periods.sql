db2de19abeea36ffa033abbe1d44c19cf9330086
Victor Ricardo Ramirez Perdomo <Victor Ricardo Ramirez Perdomo>
2016-08-03 00:35:42 -0600

    Insert Periods SQL2

--- ScriptsVinculacion/InsertData/Insersts_Periods.sql
+++ ScriptsVinculacion/InsertData/Insersts_Periods.sql
@@ -42,3 +42,4 @@ INSERT INTO `vinculacion`.`periods` (Number, Year, IsCurrent) VALUES (1, 2014, 0
 INSERT INTO `vinculacion`.`periods` (Number, Year, IsCurrent) VALUES (4, 2014, 0);
 INSERT INTO `vinculacion`.`periods` (Number, Year, FromDate, ToDate, IsCurrent) VALUES (2, 2016,STR_TO_DATE('18-04-2016', '%d-%m-%Y'),STR_TO_DATE('28-06-2016', '%d-%m-%Y'), 0);
 INSERT INTO `vinculacion`.`periods` (Number, Year, FromDate, ToDate, IsCurrent) VALUES (4, 2016,STR_TO_DATE('18-07-2016', '%d-%m-%Y'),STR_TO_DATE('27-09-2016', '%d-%m-%Y'),1);
+--comment
